a643a33105c57e5d3122d6fed297c94d92a265d8
Leence Lidoros <leencelidonde00@gmail.com>
2024-01-10 12:00:52 +0300

    Registered URLs

---
+++ confirmation_url.php
@@ -0,0 +1,17 @@
+<?php
+
+header("Content-Type:application/json");
+$response ='{
+      "ResultCode":0,
+      "ResultDesc":Confirmation Recieved Successfully"
+}';
+
+$mpesaResponse =file_get_contents('php://input');
+
+$logfile ="MpesaResponse.txt";
+$jsonMpesaResponse =json_decode($mpesaResponse,true);
+$log =fopen($logfile,"a");
+fwrite($log ,$jsonMpesaResponse);
+fclose($log);
+
+echo($response);
